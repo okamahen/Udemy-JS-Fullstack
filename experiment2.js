e21c0f33861c4126680fa81ef3a828a2b728d646
Oka Mahendra Saputra <52365870+okamahen@users.noreply.github.com>
2020-12-12 07:03:35 +0700

    Add files via upload

---
+++ experiment2.js
@@ -0,0 +1,46 @@
+let express = require("express")
+let app = express()
+//NOTE : tried to use focus to form input but does not work!
+
+//Boilerplate code, used to enable express to catch HTML body object, see line 22
+app.use(express.urlencoded({extended: false}))
+
+//get is from Express, it use 2 argument, 1st is for URL to look, 2nd is function that Express is going to run 
+app.get('/', function(req, res) {
+    //send is from Express, to send anything to the web, on this example we will send HTML syntax
+    //respond for form will redirect to /answer page, use POST method
+    res.send(`
+    <form action="/answer" method="POST">
+        <p>What color is the sky on a clear sunny day?</p>
+        <input name="userInput" autocomplete="off">
+        <button>Submit Answer</button>
+    </form>
+    `)
+})
+
+
+
+//Example for POST, to catch value on a form and send/post it to another page named "answer"
+app.post('/answer', function(req, res) {
+    //Out of box, Express will not add user input to the body object, so we need to execute line 5 (app.use)
+    //Add method toUpperCase() from JS to convert input into all upper case, to accept variable input by user
+    //To return to home, we just need to use anchor tag <a> with hyperlink (href) to "/" 
+    if (req.body.userInput.toUpperCase() == "BLUE") {
+        res.send(`
+        <p> Congrats, ${req.body.userInput} is the correct answer</p>
+        <a href="/">Back to homepage</a>
+        `)
+    } else {
+        res.send(`
+        <p> ${req.body.userInput} is a wrong answer! please try again.</p>
+        <a href="/">Back to homepage</a>
+        `)
+    }
+})
+
+//Example for GET, for accessing another page by writing in address bar to page named "answer", can also work for navigation link
+app.get('/answer', function(req, res) {
+    res.send('GET request performed, answer not submitted.')
+})
+
+app.listen(3000)
